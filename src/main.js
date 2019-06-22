0fb17ec24e79e5a0caeec6d7f6f37b6a793a262c
Ali <ali.doostkany@gmail.com>
2019-06-22 18:23:28 +0430

    add fullscreen button

--- src/main.js
+++ src/main.js
@@ -66,3 +66,29 @@ wireClick("ol", () => richtext.applyOrderedList());
 wireClick("ul", () => richtext.applyUnorderedList());
 wireClick("image", () => richtext.selectImage());
 wireClick("link", () => richtext.styleLink());
+wireClick("fullscreen", fullscreen);
+
+function fullscreen() {
+  const header = document.querySelector("header").style;
+  header.display = toggle(header.display, "block", "none");
+
+  const tabs = document.querySelector("main>div").style;
+  tabs.display = toggle(tabs.display, "block", "none");
+
+  const main = document.querySelector("main").style;
+  main.minWidth = toggle(main.minWidth, "60%", "90%");
+
+  const richtextArea = document.querySelector("#richtext-area").style;
+  richtextArea.marginTop = toggle(richtextArea.marginTop, "20px", "");
+
+  const fullscreen = document.querySelector("#fullscreen>i");
+  fullscreen.className = toggle(
+    fullscreen.className,
+    "fas fa-arrows-alt",
+    "fas fa-compress-arrows-alt"
+  );
+}
+
+function toggle(style, value, other) {
+  return style === "" || style === value ? other : value;
+}
